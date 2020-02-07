873815cb4915b1fe248e1b04922e74d8cae9d0b2
Niklas Grieger <niklas.grieger1@gmail.com>
2020-02-08 00:47:22 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Because we otherwise get error on heroku database - clear db
+        //Error: Specified key was too long error
+        Schema::defaultStringLength(191);
     }
 }
