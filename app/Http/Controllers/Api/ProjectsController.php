3bf19d63d1d285544bdd57d36a0e118b1eec0d03
Niklas Grieger <niklas.grieger1@gmail.com>
2020-03-17 09:53:00 +0100

    add migration

--- app/Http/Controllers/Api/ProjectsController.php
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -9,7 +9,7 @@ use App\Models\Project;
 class ProjectsController
 {
     public function get(){
-        $projects = Project::query()->orderByDesc('updated_at')->get();
+        $projects = Project::query()->orderBy('priority')->get();
         foreach ($projects as $project){
             $project['icons'] = $project->icons;
         }
